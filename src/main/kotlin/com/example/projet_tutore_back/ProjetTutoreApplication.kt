6455f0935751d262db1a8423168dba006af43897
Thomas KLING <klingthomas67@gmail.com>
2021-04-28 14:25:56 +0200

    init projet

---
+++ src/main/kotlin/com/example/projet_tutore_back/ProjetTutoreApplication.kt
@@ -0,0 +1,11 @@
+package com.example.projet_tutore_back
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ProjetTutoreApplication
+
+fun main(args: Array<String>) {
+    runApplication<ProjetTutoreApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/projet_tutore_back/ProjetTutoreApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.projet_tutore_back
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ProjetTutoreApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
